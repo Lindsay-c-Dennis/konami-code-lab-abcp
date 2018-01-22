a9d1c7e0895959de60b1e4aa658cefd74d811230
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-01-22 22:57:05 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,9 +3,11 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
 keys = [];
 
-document.addEventListener("keydown" keysPressed)
-function keysPressed(e) {
+document.addEventListener("keydown" keysPressed(e) {
   keys.push(e.which);
+}
+if (keys === code) {
+  alert("Hooray!");
 }
   
   
