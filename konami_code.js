1fe7d27237cb9e952a4bd5ada83a71b5aca8f176
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-01-22 22:52:38 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,24 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-let index = 0;
+keys = [];
 
-function  onKeyDownHandler(e) {
-  const key = parseInt(event.which);
-  
-  if (key === code[index]) {
-    index++;
-    
-    if (index===code.length) {
-      alert("Hurray!");
-      
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
-}
-
-document.addEventListener("Keydown", onKeyDownHandler(e));
   
 }
